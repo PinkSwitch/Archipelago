31b686832b1e477d23e9d5ad4a7842fe87b4646a
PinkSwitch <blabidyfa@gmail.com>
2024-10-22 08:21:06 -0500

    hint styling

--- worlds/earthbound/hint_data.py
+++ worlds/earthbound/hint_data.py
@@ -10,5 +10,28 @@ def setup_hints(world):
     ]
     world.in_game_hint_types = []
 
+    local_hintable_locations = [
+        "Lost Underworld - Talking Rock",
+        "Dungeon Man - 2F Hole Present",
+        "Poo Starting Item",
+        "Summers - Magic Cake",
+        "Onett - Mayor Pirkle",
+        "Twoson - Orange Kid Donation",
+        "Twoson - Everdred Meeting",
+        "Onett - South Road Present",
+        "Deep Darkness - Teleporting Monkey"
+    ]
+
     for i in range(6):
         world.in_game_hint_types.append(world.random.choice(hint_types))
+
+    print(world.in_game_hint_types)
+
+
+    #Word on the street is that PLAYER's ITEM can be found at LOCATION
+    #Word on the street is that REGION may be hiding a critical item./may be hiding an import-sounding item./may have nothing of much conseuqence.
+    #Word on the street is that your ITEM can be found by PLAYER at LOCATION
+    #Word on the street is that ITEM can be found somewhere near REGION...
+    #Word on the street is that your ITEM can be found somewhere near REGION...
+    #char item hint?
+    #That's all for today.
