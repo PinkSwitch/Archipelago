167d316907af3b5663b3aabdb487ebc28672e440
PinkSwitch <blabidyfa@gmail.com>
2024-11-03 21:26:53 -0600

    fix starstorm name

--- worlds/earthbound/psi_shuffle.py
+++ worlds/earthbound/psi_shuffle.py
@@ -418,13 +418,7 @@ def write_psi(world, rom):
             elif psi_num == 5 and i > 1:
                 rom.write_bytes(0x01C4AB + (0x9E * (i - 2)), struct.pack("H", world.starstorm_address[spell][i - 2]))
                 rom.write_bytes(0x01C536 + (0x78 * (i - 2)), bytearray([world.starstorm_spell_id[spell][i - 2]]))
-                #Write the ID of PSI Starstorm into dialogue for receiving Progressive Poo PSI.
-                #The reason it's done like this is because starstorm's slot only has the 2 upper slots (so the id is i - 2)
-                #and it writes to a base address + the byte difference, will be *0 for level 1 and * 1 for level 2, so it
-                #writes correctly during each loop
-                #come to think of it i might be able to clean this up with a manual lookup
-                #rom.write_bytes(0x2E957F + (0x11 * (i - 2)), bytearray([world.starstorm_spell_id[spell][i - 2]]))
-                #rom.write_bytes(0x2EAE2E + (0x0A * (i - 2)), bytearray([world.starstorm_spell_id[spell][i - 2]])) #Local texts one of these might still be worth keeping around
+                rom.write_bytes(0x2E957F + (0x11 * (i - 2)), bytearray([world.starstorm_spell_id[spell][i - 2]]))
                 rom.write_bytes(address + 9, bytearray(world.psi_slot_data[psi_num][i - 2]))
 
             if spell == "Special" and psi_num != 0:
