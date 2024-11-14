14b0fb958dbc5cac94a76049e539c5dfa23fb7fd
PinkSwitch <blabidyfa@gmail.com>
2024-11-13 21:35:24 -0600

    equipamizer names

--- worlds/earthbound/equipamizer.py
+++ worlds/earthbound/equipamizer.py
@@ -3,7 +3,7 @@ from typing import Dict
 
 def roll_resistances(world, element, armor):
     chance = world.random.randint(0, 100)
-    if chance < 15:
+    if chance < 95:
         setattr(armor, element, world.random.randint(1, 3))
     else:
         setattr(armor, element, 0)
@@ -70,8 +70,40 @@ def randomize_armor(world):
         "Silence",
         "Ulitmate",
         "Charm",
-        "Saturn"
+        "Saturn",
+        "Tenda"
     ]
+
+    equalized_names = [
+        "???",
+        "Earth",
+        "Sea"
+    ]
+
+    ult_names = [
+        "???",
+        "Sun",
+        "Star"
+    ]
+
+    elemental_names = {
+        "Flash": [
+            "???",
+            "Cloud",
+            "Night"
+        ],
+        "Freeze": [
+            "Puddle",
+            "???",
+            "Rain"
+        ],
+
+        "Fire": [
+            "Smoke",
+            "Ember",
+            "Flame"
+        ]
+    }
 
     all_armor = [
         "Cheap Bracelet"
@@ -97,17 +129,34 @@ def randomize_armor(world):
 
     for item in all_armor:
         armor = world.armor_list[item]
-        armor.name = "Butts"
         armor.defense = world.random.randint(1,127)
 
-        #make this a def? or a loop at least
         roll_resistances(world, "flash_res", armor)
         roll_resistances(world, "freeze_res", armor)
         roll_resistances(world, "fire_res", armor)
         roll_resistances(world, "par_res", armor)
 
+        if armor.flash_res + armor.freeze_res + armor.fire_res == 0:
+            #If no resistances are active use a normal name
+            front_name = world.random.choice(adjectives)
+        elif armor.flash_res == armor.freeze_res == armor.fire_res:
+            #Get a combined name for the level
+            front_name = equalized_names[armor.flash_res - 1]
+        elif armor.par_res == armor.flash_res == armor.freeze_res == armor.fire_res:
+            #Should be used if Paralysis + the others all succeed
+            front_name = ult_names[armor.flash_res - 1]
+        else:
+            #If resistances are inequal, use the strongest as the name and pull a name from its strength
+            #If 2 are equal pick a random of them
+            names = ("Flash", "Freeze", "Fire")
+            strengths = (armor.flash_res, armor.freeze_res, armor.fire_res)
+            best_elements = [(name, strength) for name, strength in zip(names, strengths, strict=True) if strength == max(strengths)]
+            best_name, best_strength = world.random.choice(best_elements)
+            front_name = elemental_names[best_name][best_strength - 1]
+
+
         chance = world.random.randint(0,100)
-        if chance < 20:
+        if chance < 10:
             armor.only_char = world.random.choice(["Ness", "Paula", "Jeff", "Poo"])
         else:
             armor.only_char = "None"
