e25692cf5d360242af0d08be56d70dc91f31e7cc
PinkSwitch <blabidyfa@gmail.com>
2025-01-28 00:12:24 -0600

    fix fail again

--- worlds/earthbound/modules/enemy_data.py
+++ worlds/earthbound/modules/enemy_data.py
@@ -464,7 +464,7 @@ spell_breaks: Dict[str, Dict[int, str]] = {
     "starstorm_minus": {12: "zeta", 20: "epsilon", 45: "delta", 70: "lambda", 100: "alpha", 200: "beta"},
     "flash_minus": {60: "alpha", 70: "beta", 100: "gamma", 200: "omega"},
     "blast": {10: "zeta", 20: "epsilon", 30: "alpha", 40: "beta", 50: "gamma", 100: "omega"},
-    "missile": {5: "zeta", 12: "epsilon": 20: "alpha", 50: "beta", 73: "gamma", 100: "omega"},
+    "missile": {5: "zeta", 12: "epsilon", 20: "alpha", 50: "beta", 73: "gamma", 100: "omega"},
     # Todo; blast/missile delta and lambda
     # "blast": {00: "delta", 20: "lambda", 20: "alpha", 35: "beta", 45: "gamma", 100: "omega"},
 
