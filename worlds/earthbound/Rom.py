6988fb8b43afe5b3ba216e27edcfb59e53e967e5
PinkSwitch <blabidyfa@gmail.com>
2025-03-08 09:23:35 -0600

    Teleport cleanup

--- worlds/earthbound/Rom.py
+++ worlds/earthbound/Rom.py
@@ -79,7 +79,11 @@ class LocalRom(object):
 
 def patch_rom(world, rom, player: int):
     rom.copy_bytes(0x1578DD, 0x3D, 0x1578FC) # Threed/Saturn teleport move
-    rom.copy_bytes(0x15791B, 0xF7, ???)
+    rom.copy_bytes(0x15791B, 0xF7, 0x157959)
+
+    rom.copy_bytes(0x340000, 0x1E, 0x1578DD)
+    rom.copy_bytes(0x340020, 0x1E, 0x15793A)
+    rom.copy_bytes(0x340040, 0x1E, 0x157A51)
 
     starting_area_coordinates = {
                     0: [0x50, 0x04, 0xB5, 0x1F],  # North Onett
