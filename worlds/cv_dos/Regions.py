913075de4270cbc32a149dc9c1909c3ba7c343d1
PinkSwitch <blabidyfa@gmail.com>
2025-12-18 09:34:02 -0600

    Separate Soul regions

--- worlds/cv_dos/Regions.py
+++ worlds/cv_dos/Regions.py
@@ -2,6 +2,7 @@ from typing import List, Dict, TYPE_CHECKING
 from BaseClasses import Region, Location
 from .Locations import LocationData
 from .Rules import small_uppies, big_uppies
+from .soul_regions import create_soul_regions
 if TYPE_CHECKING:
     from . import DoSWorld
 
@@ -91,17 +92,14 @@ def init_areas(world: "DoSWorld", locations: List[LocationData]) -> None:
         create_region(world, player, locations_per_region, "Warp Room"),
     ]
 
-    if world.options.soul_randomizer == 2:
-        for region in world.common_souls:
-            regions.append(create_region(world, player, locations_per_region, region))
+    for region in world.common_souls:
+        regions.append(create_region(world, player, locations_per_region, region))
 
-        if world.options.soulsanity_level:
-            for region in world.uncommon_souls:
-                regions.append(create_region(world, player, locations_per_region, region))
+    for region in world.uncommon_souls:
+        regions.append(create_region(world, player, locations_per_region, region))
 
-        if world.options.soulsanity_level == 2:
-            for region in world.rare_souls:
-                regions.append(create_region(world, player, locations_per_region, region))  
+    for region in world.rare_souls:
+        regions.append(create_region(world, player, locations_per_region, region))
 
     multiworld.regions += regions
     ########################################
@@ -303,254 +301,13 @@ def init_areas(world: "DoSWorld", locations: List[LocationData]) -> None:
     multiworld.get_region("The Abyss Beyond Abaddon", player).add_exits(["Warp Room"])
 
     multiworld.get_region("Warp Room", player).add_exits([world.starting_warp_region])
-    #Maybe I should make Bone Ark progression anyways? So you can be expected to use it from the left side...
+    multiworld.get_region("Subterranean Hell Spike Room East", player).add_exits(["Subterranean Hell Spike Room West"],
+                            {"Subterranean Hell Spike Room West": lambda state: state.has("Rahab Soul", player) and (state.has_all({"Puppet Master Soul", "Skeleton Ape Soul"}, player) or state.has("Bone Ark Soul", player))})
 
-    if world.options.soul_randomizer == 2:
-        multiworld.get_region("Subterranean Hell Spike Room East", player).add_exits(["Subterranean Hell Spike Room West"],
-                                {"Subterranean Hell Spike Room West": lambda state: state.has("Rahab Soul", player) and (state.has_all({"Puppet Master Soul", "Skeleton Ape Soul"}, player) or state.has("Bone Ark Soul", player))})
+    multiworld.get_region("Subterranean Hell Spike Room West", player).add_exits(["Subterranean Hell Spike Room East"],
+                            {"Subterranean Hell Spike Room East": lambda state: state.has("Rahab Soul", player) and (state.has_all({"Puppet Master Soul", "Skeleton Ape Soul"}, player) or state.has("Bone Ark Soul", player))})
 
-        multiworld.get_region("Subterranean Hell Spike Room West", player).add_exits(["Subterranean Hell Spike Room East"],
-                                {"Subterranean Hell Spike Room East": lambda state: state.has("Rahab Soul", player) and (state.has_all({"Puppet Master Soul", "Skeleton Ape Soul"}, player) or state.has("Bone Ark Soul", player))})
-
-        multiworld.get_region("Lost Village Upper", player).add_exits(["Yeti Soul", "Axe Armor Soul", "Warg Soul", "Spin Devil Soul"],
-        {"Spin Devil Soul": lambda state: state.has("Moat Drained", player),
-         "Yeti Soul": lambda state: state.has("Waiter Skeleton Soul", player)})
-
-        multiworld.get_region("Lost Village Lower", player).add_exits(["Spin Devil Soul"])
-
-        multiworld.get_region("Lost Village Courtyard", player).add_exits(["Warg Soul"])
-
-        multiworld.get_region("Lost Village Underground Top", player).add_exits(["Axe Armor Soul", "Merman Soul"])
-        ####### WIZARDRY LAB SOULS ########
-        multiworld.get_region("Wizardry Lab Main", player).add_exits(["Slime Soul", "Axe Armor Soul"])
-
-        multiworld.get_region("Wizardry Lab East Gate", player).add_exits(["Cave Troll Soul"])
-
-        multiworld.get_region("Wizardry Lab Sunken", player).add_exits(["Homunculus Soul", "Killer Fish Soul"])
-        ####### GARDEN OF MADNESS ######
-        multiworld.get_region("Garden of Madness Lower", player).add_exits(["Corpseweed Soul"])
-
-        multiworld.get_region("Garden of Madness Upper", player).add_exits(["Corpseweed Soul"])
-
-        multiworld.get_region("Garden of Madness Post-Boss", player).add_exits(["Corpseweed Soul"])
-        ##### DEMON GUEST HOUSE ####
-        multiworld.get_region("Demon Guest House Lower", player).add_exits(["Axe Armor Soul"])
-
-        multiworld.get_region("Demon Guest House West Wing", player).add_exits(["Buer Soul"])
-        ##### DARK CHAPEL #####
-
-        multiworld.get_region("Dark Chapel", player).add_exits(["Guillotiner Soul"])
-        #####CONDEMNED TOWER #####
-        multiworld.get_region("Condemned Tower Top", player).add_exits(["Buer Soul"])
-        ##### CURSED CLOCK TOWER ######
-        multiworld.get_region("Cursed Clock Tower Central", player).add_exits(["Slime Soul"])
-
-        multiworld.get_region("Cursed Clock Tower Boss Area", player).add_exits(["Flying Humanoid Soul"],
-        {"Flying Humanoid Soul": lambda state: state.has("Mandragora Soul", player)})
-        ##### SUBTERRANEAN HELL #####
-        multiworld.get_region("Subterranean Hell Top Entrance", player).add_exits(["Cave Troll Soul", "Decarabia Soul"],
-        {"Decarabia Soul": lambda state: state.has("Magic Seal 3", player)})
-
-        multiworld.get_region("Subterranean Hell East", player).add_exits(["Decarabia Soul", "Merman Soul"],
-        {"Decarabia Soul": lambda state: state.has("Rahab Soul", player)})
-
-        multiworld.get_region("Subterranean Hell Shaft Bottom", player).add_exits(["Merman Soul"])
-
-        multiworld.get_region("Subterranean Hell Central Lower", player).add_exits(["Merman Soul"])
-
-        multiworld.get_region("Subterranean Hell Central Upper", player).add_exits(["Merman Soul"])
-
-        multiworld.get_region("Subterranean Hell Central Exit", player).add_exits(["Merman Soul"])
-        ####S ILENCED RUINS #####
-        multiworld.get_region("Silenced Ruins", player).add_exits(["Dead Mate Soul"])
-        ##### PINNACLE #####
-        multiworld.get_region("The Pinnacle", player).add_exits(["Guillotiner Soul", "Mothman Soul"],
-        {"Mothman Soul": lambda state: state.has("Rycuda Soul", player)})
-
-        multiworld.get_region("The Pinnacle Lower", player).add_exits(["Guillotiner Soul"])
-
-        if world.options.goal:
-            multiworld.get_region("Mine of Judgment", player).add_exits(["Slogra Soul"])
-
-            multiworld.get_region("The Abyss Beyond Abaddon", player).add_exits(["Black Panther Soul"])
-
-        if world.options.soulsanity_level:
-            multiworld.get_region("Lost Village Upper", player).add_exits(["Skeleton Soul", "Bat Soul", "Armor Knight Soul", "Zombie Soul"],
-            {"Spin Devil Soul": lambda state: state.has("Moat Drained", player),
-             "Armor Knight Soul": lambda state: state.has_any(big_uppies, player)})
-
-            multiworld.get_region("Lost Village Upper Doorway", player).add_exits(["Skelerang Soul"])
-
-            multiworld.get_region("Lost Village Lower", player).add_exits(["Skeleton Soul", "Armor Knight Soul", "Bat Soul", "Zombie Soul", "Student Witch Soul"],
-            {"Student Witch Soul": lambda state: state.has("Moat Drained", player)}) #It's in the same area as the switch
-
-            multiworld.get_region("Lost Village Courtyard", player).add_exits(["Hell Boar Soul"],
-            {"Hell Boar Soul": lambda state: state.has_any(big_uppies, player)})
-
-            multiworld.get_region("Lost Village Underground Bottom", player).add_exits(["White Dragon Soul"])
-            ####### WIZARDRY LAB SOULS ########
-            multiworld.get_region("Wizardry Lab Main", player).add_exits(["Bomber Armor Soul", "Student Witch Soul", "Skull Archer Soul", "Skeleton Soul", "Slaughterer Soul", "Manticore Soul", "Armor Knight Soul", "Golem Soul"])
-
-            multiworld.get_region("Wizardry Lab Sunken", player).add_exits(["Larva Soul"])
-            ####### GARDEN OF MADNESS ######
-            multiworld.get_region("Garden of Madness Lower", player).add_exits(["Une Soul", "Skelerang Soul", "Mandragora Soul", "Catoblepas Soul", "Mollusca Soul", "Yorick Soul", "Rycuda Soul"])
-
-            multiworld.get_region("Garden of Madness Upper", player).add_exits(["Rycuda Soul", "Mollusca Soul"])
-
-            multiworld.get_region("Garden of Madness Post-Boss", player).add_exits(["Skelerang Soul", "Une Soul"])
-
-            multiworld.get_region("Garden of Madness East Gate", player).add_exits(["Wakwak Tree Soul"])
-            ##### DEMON GUEST HOUSE ####
-            multiworld.get_region("Demon Guest House Lower", player).add_exits(["Skeleton Soul"])
-
-            multiworld.get_region("Demon Guest House Puppet Wall Right", player).add_exits(["Skelerang Soul"])
-
-            multiworld.get_region("Demon Guest House Main", player).add_exits(["Persephone Soul", "Skelerang Soul", "Devil Soul", "Lilith Soul", "Ghost Dancer Soul", "Killer Clown Soul", "Waiter Skeleton Soul"])
-
-            multiworld.get_region("Demon Guest House Number Puzzle West", player).add_exits(["Persephone Soul"])
-
-            multiworld.get_region("Demon Guest House West Wing", player).add_exits(["Killer Clown Soul", "Lilith Soul", "Quetzalcoatl Soul"])
-
-            multiworld.get_region("Demon Guest House Upper", player).add_exits(["Flame Demon Soul", "Malachi Soul", "Skelerang Soul", "Werewolf Soul", "Ghost Dancer Soul", "Student Witch Soul", "Lilith Soul", "Witch Soul", "Succubus Soul", "Persephone Soul"])
-            ##### DARK CHAPEL #####
-
-            multiworld.get_region("Dark Chapel", player).add_exits(["Witch Soul", "Mini Devil Soul", "Amalaric Sniper Soul", "Ghost Dancer Soul", "Hell Boar Soul", "White Dragon Soul", "Great Armor Soul", "Quetzalcoatl Soul"],
-            {"Quetzalcoatl Soul": lambda state: state.has("Magic Seal 2", player)})
-
-            multiworld.get_region("Dark Chapel Big Room", player).add_exits(["Mini Devil Soul", "Quetzalcoatl Soul"])
-
-            multiworld.get_region("Dark Chapel Catacombs Exit", player).add_exits(["Catoblepas Soul"])
-            #####CONDEMNED TOWER #####
-
-            multiworld.get_region("Condemned Tower Top", player).add_exits(["Disc Armor Soul", "Werewolf Soul", "Fleaman Soul"])
-            ##### CURSED CLOCK TOWER ######
-            multiworld.get_region("Cursed Clock Tower Entrance", player).add_exits(["Harpy Soul", "Catoblepas Soul", "Imp Soul", "Malachi Soul", "Dead Pirate Soul"])
-
-            multiworld.get_region("Cursed Clock Tower Central", player).add_exits(["Imp Soul"])
-
-            multiworld.get_region("Cursed Clock Tower Exit", player).add_exits(["Devil Soul", "Harpy Soul"])
-            ##### SUBTERRANEAN HELL #####
-            multiworld.get_region("Subterranean Hell Top Entrance", player).add_exits(["Une Soul", "Dead Pirate Soul"])
-
-            multiworld.get_region("Subterranean Hell East", player).add_exits(["Fish Head Soul", "Needles Soul", "Frozen Shade Soul", "Killer Fish Soul"],
-            {"Fish Head Soul": lambda state: state.has("Rahab Soul", player),
-            "Needles Soul": lambda state: state.has("Rahab Soul", player)})
-
-            multiworld.get_region("Subterranean Hell Central/East Connection", player).add_exits(["Une Soul", "Alura Une Soul"])
-
-            multiworld.get_region("Subterranean Hell Shaft Bottom", player).add_exits(["Dead Pirate Soul", "Ukoback Soul"])
-
-            multiworld.get_region("Subterranean Hell Shaft Middle", player).add_exits(["Frozen Shade Soul", "Devil Soul"])
-
-            multiworld.get_region("Subterranean Hell Shaft Top", player).add_exits(["Killer Fish Soul", "Ukoback Soul"])
-
-            multiworld.get_region("Subterranean Hell Central Lower", player).add_exits(["Frozen Shade Soul", "Dead Pirate Soul"])
-
-            multiworld.get_region("Subterranean Hell Central Upper", player).add_exits(["Ukoback Soul", "Frozen Shade Soul", "Dead Pirate Soul", "Killer Fish Soul", "Needles Soul"],
-                               {"Needles Soul": lambda state: state.has("Rahab Soul", player),
-                                "Killer Fish Soul": lambda state: state.has("Rahab Soul", player)})
-
-            multiworld.get_region("Subterranean Hell Central Exit", player).add_exits(["Ukoback Soul", "Frozen Shade Soul"])
-            ####S ILENCED RUINS #####
-            multiworld.get_region("Silenced Ruins", player).add_exits(["Skeleton Soul", "Bat Soul", "Skull Archer Soul", "Devil Soul", "Larva Soul", "Skelerang Soul"],
-            {"Devil Soul": lambda state: state.has("Balore Soul", player)})
-
-            multiworld.get_region("Silenced Ruins Back Exit", player).add_exits(["Waiter Skeleton Soul"])
-            ##### PINNACLE #####
-            multiworld.get_region("The Pinnacle", player).add_exits(["Werewolf Soul", "Mushussu Soul", "Alastor Soul"])
-
-            multiworld.get_region("The Pinnacle Lower", player).add_exits(["Succubus Soul", "Malachi Soul", "Mushussu Soul", "Werewolf Soul", "Flame Demon Soul"])
-
-            if world.options.goal:
-                multiworld.get_region("Mine of Judgment", player).add_exits(["Ripper Soul", "Gaibon Soul"])
-
-                multiworld.get_region("The Abyss", player).add_exits(["Alastor Soul", "Mud Demon Soul", "Frozen Shade Soul", "Malachi Soul", "White Dragon Soul", "Malacoda Soul"])
-
-                multiworld.get_region("The Abyss Beyond Abaddon", player).add_exits(["Succubus Soul"])
-
-        if world.options.soulsanity_level == 2:
-            # Rare souls
-            multiworld.get_region("Lost Village Upper", player).add_exits(["Peeping Eye Soul"])
-
-            multiworld.get_region("Lost Village Upper Doorway", player).add_exits(["Peeping Eye Soul"])
-
-            multiworld.get_region("Lost Village Lower", player).add_exits(["Ouija Table Soul"])
-
-            multiworld.get_region("Lost Village Courtyard", player).add_exits(["Skeleton Ape Soul"])
-
-            multiworld.get_region("Lost Village Underground Top", player).add_exits(["Great Axe Armor Soul"])
-            ####### WIZARDRY LAB SOULS ########
-            multiworld.get_region("Wizardry Lab Main", player).add_exits(["Ghost Soul", "The Creature Soul"])
-
-            multiworld.get_region("Wizardry Lab West Gate", player).add_exits(["Great Axe Armor Soul", "Heart Eater Soul"])
-
-            multiworld.get_region("Wizardry Lab East Gate", player).add_exits(["Mimic Soul"])
-
-            multiworld.get_region("Wizardry Lab Sunken", player).add_exits(["Mimic Soul"])
-
-            multiworld.get_region("Wizardry Lab Sunken West Door", player).add_exits(["Iron Golem Soul"])
-            ####### GARDEN OF MADNESS ######
-            multiworld.get_region("Garden of Madness Lower", player).add_exits(["Treant Soul", "Skeleton Ape Soul", "Skeleton Farmer Soul", "Mimic Soul"],
-            {"Mimic Soul": lambda state: state.has_any(small_uppies, player)})
-
-            multiworld.get_region("Garden of Madness Upper", player).add_exits(["Barbariccia Soul", "Skeleton Ape Soul", "Treant Soul"])
-
-            multiworld.get_region("Garden of Madness Post-Boss", player).add_exits(["Skeleton Ape Soul", "Ghoul Soul"])
-            ##### DEMON GUEST HOUSE ####
-            multiworld.get_region("Demon Guest House Lower", player).add_exits(["Peeping Eye Soul"])
-
-            multiworld.get_region("Demon Guest House Main", player).add_exits(["Valkyrie Soul", "Killer Doll Soul", "Bone Pillar Soul"])
-
-            multiworld.get_region("Demon Guest House West Wing", player).add_exits(["Killer Doll Soul", "Bone Pillar Soul"])
-
-            multiworld.get_region("Demon Guest House Upper", player).add_exits(["Iron Golem Soul", "Mimic Soul"])
-            ##### DARK CHAPEL #####
-
-            multiworld.get_region("Dark Chapel", player).add_exits(["Ghoul Soul", "The Creature Soul", "Bone Pillar Soul", "Barbariccia Soul", "Valkyrie Soul", "Ghost Soul", "Tombstone Soul"])
-
-            multiworld.get_region("Dark Chapel Big Room", player).add_exits(["Valkyrie Soul"])
-
-            #####CONDEMNED TOWER #####
-            multiworld.get_region("Condemned Tower Bottom", player).add_exits(["Draghignazzo Soul"])
-
-            multiworld.get_region("Condemned Tower Top", player).add_exits(["Skeleton Ape Soul", "Great Axe Armor Soul"])
-            ##### CURSED CLOCK TOWER ######
-            multiworld.get_region("Cursed Clock Tower Entrance", player).add_exits(["Medusa Head Soul", "Tanjelly Soul"])
-
-            multiworld.get_region("Cursed Clock Tower Central", player).add_exits(["Medusa Head Soul", "Bugbear Soul", "Tanjelly Soul"])
-
-            ##### SUBTERRANEAN HELL #####
-            multiworld.get_region("Subterranean Hell East", player).add_exits(["Mimic Soul", "Procel Soul"],
-            {"Mimic Soul": lambda state: state.has("Rahab Soul", player)})
-
-            multiworld.get_region("Subterranean Hell Shaft Bottom", player).add_exits(["Medusa Head Soul"])
-
-            multiworld.get_region("Subterranean Hell Central Lower", player).add_exits(["Procel Soul"])
-
-            multiworld.get_region("Subterranean Hell Central Upper", player).add_exits(["Bone Ark Soul"])
-
-            multiworld.get_region("Subterranean Hell Central Exit", player).add_exits(["Mimic Soul"])
-            ####S ILENCED RUINS #####
-            multiworld.get_region("Silenced Ruins", player).add_exits(["Ghoul Soul", "Peeping Eye Soul", "Dead Crusader Soul", "Bone Ark Soul"])
-
-            multiworld.get_region("Silenced Ruins Back Exit", player).add_exits(["Gorgon Soul"])
-            ##### PINNACLE #####
-            multiworld.get_region("The Pinnacle", player).add_exits(["Dead Crusader Soul", "Erinys Soul", "Final Guard Soul"])
-
-            multiworld.get_region("The Pinnacle Lower", player).add_exits(["Bugbear Soul", "Dead Warrior Soul", "Erinys Soul"])
-
-            if world.options.goal:
-                multiworld.get_region("Mine of Judgment", player).add_exits(["Tanjelly Soul", "Giant Slug Soul", "Bugbear Soul"])
-
-                multiworld.get_region("The Abyss", player).add_exits(["Arc Demon Soul", "Erinys Soul", "Heart Eater Soul", "Stolas Soul", "Final Guard Soul"])
-
-                multiworld.get_region("The Abyss Beyond Abaddon", player).add_exits(["Iron Golem Soul"])
-    else:
-        # If no soulsanity you can grind Bone ark for this
-        multiworld.get_region("Subterranean Hell Spike Room East", player).add_exits(["Subterranean Hell Spike Room West"]) # Or bone ark...
-
-        multiworld.get_region("Subterranean Hell Spike Room West", player).add_exits(["Subterranean Hell Spike Room East"])
-
+    create_soul_regions(world)
 
 def create_location(player: int, location_data: LocationData, region: Region) -> Location:
     location = DoSLocation(player, location_data.name, location_data.code, region)
