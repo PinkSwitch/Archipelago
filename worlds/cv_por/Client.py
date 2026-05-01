10d0003e0bd24553281dcd5586e0a149305b1d1e
PinkSwitch <blabidyfa@gmail.com>
2026-04-30 23:23:20 -0500

    Add ROM authentication

--- worlds/cv_por/Client.py
+++ worlds/cv_por/Client.py
@@ -2,6 +2,7 @@ from typing import TYPE_CHECKING
 import worlds._bizhawk as bizhawk
 from worlds._bizhawk.client import BizHawkClient
 from .Rom import world_version
+import struct
 
 if TYPE_CHECKING:
     from worlds._bizhawk.context import BizHawkClientContext
@@ -24,19 +25,38 @@ class PoRClient(BizHawkClient):
             if game_id != "CASTLEVANIA2ACBEA4":
                 return False  # Only check Portrtait roms
 
-            validation_data = await bizhawk.read(ctx.bizhawk_ctx, [(0x308F20, 0x20, "Main RAM")])
+            validation_data = await bizhawk.read(ctx.bizhawk_ctx, [(0x308F20, 0x20, "Main RAM"), (0x0E537C, 0x04, "Main RAM")])
+            vanilla_check = struct.unpack("I", validation_data[1])[0]  # post-behemoth entity pointer
+            if vanilla_check == 0x2304ee8:  # rando changes this; if we get this value, it's vanilla
+                if self.most_recent_connect != "Vanilla ROM":
+                    ctx.gui_error("Unrandomized ROM", f"Loaded ROM appears to be unmodified. Please load a Castlevania: Portrait of Ruin Archipelago ROM.")
+                    self.most_recent_connect = "Vanilla ROM"
+                return False
+
             patch_version = validation_data[0]
             patch_version = patch_version[0x15:].split(bytes(1), 1)[0].decode("ascii")
             if patch_version != self.client_version:
                 if patch_version != self.most_recent_connect:
                     # We only want to display this error once
-                    ctx.gui_error("Bad Version", f"Installed APworld for Portrait of ruin is {self.client_version}.")
+                    ctx.gui_error("Bad Version", f"Installed APWorld for Portrait of Ruin is {self.client_version}. ROM was generated using version {patch_version}.")
                     self.most_recent_connect = patch_version
+                return False
+
+            ctx.game = self.game
+            ctx.items_handling = 0b101
+            return True
 
         except UnicodeDecodeError:
             return False
         except bizhawk.RequestFailedError:
             return False  # Should verify on the next pass
+
+    async def set_auth(self, ctx: "BizHawkClientContext") -> None:
+        slot_name_bytes = await bizhawk.read(
+            ctx.bizhawk_ctx, [(0x308F20, 0x14, "Main RAM")])
+
+        slot_name_bytes = slot_name_bytes[0].rstrip(b'\x00')
+        ctx.auth = slot_name_bytes.decode("ascii")
 
 
     async def game_watcher(self, ctx: "BizHawkClientContext") -> None:
