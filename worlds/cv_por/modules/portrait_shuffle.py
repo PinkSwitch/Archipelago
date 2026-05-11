28a9a23318ebcfcd6634f0da1c2249521297b23c
PinkSwitch <blabidyfa@gmail.com>
2026-05-10 19:07:31 -0500

    Finish portrait gfx

--- worlds/cv_por/modules/portrait_shuffle.py
+++ worlds/cv_por/modules/portrait_shuffle.py
@@ -1,5 +1,6 @@
 from ..Options import PortraitShuffle
 from typing import NamedTuple
+from dataclasses import dataclass
 import struct
 
 
@@ -8,6 +9,43 @@ class PortraitData(NamedTuple):
     destination_map: int  # Which map this leads you to
     destination_room: int  # Which room you land in
     spoiler_map_name: str = "None"
+
+
+area_map = [
+    "City of Haze",
+    "13th Street",
+    "Sandy Grave",
+    "Forgotten City",
+    "Nation of Fools",
+    "Burnt Paradise",
+    "Forest of Doom",
+    "Dark Academy",
+    "Nest of Evil"
+]
+
+sprite_order = [
+    "City of Haze",
+    "Sandy Grave",
+    "Nation of Fools",
+    "Forest of Doom",
+    "13th Street",
+    "Forgotten City",
+    "Burnt Paradise",
+    "Dark Academy",
+    "Nest of Evil"
+]
+
+palette_order = [
+    "City of Haze",
+    "Sandy Grave",
+    "Forest of Doom",
+    "Nation of Fools",
+    "13th Street",
+    "Forgotten City",
+    "Dark Academy",
+    "Burnt Paradise",
+    "Nest of Evil"
+]
 
 
 portrait_data = {
@@ -121,114 +159,73 @@ def write_portrait_data(world, rom) -> None:
 
 
 def adjust_portrait_gfx(rom):
-    city_of_haze = []
-    sandy_grave = []
-    nation_of_fools = []
-    forest_of_doom = []
-    thirteenth_street = []
-    forgotten_city = []
-    burnt_paradise = []
-    dark_academy = []
-    nest_of_evil = []
+    @dataclass
+    class PortraitGFX:
+        sprite: list
+        palette: list
+        destination_map: int = 0
 
-    portrait_order = [
-        city_of_haze,
-        sandy_grave,
-        nation_of_fools,
-        forest_of_doom,
-
-        thirteenth_street,
-        forgotten_city,
-        burnt_paradise,
-        dark_academy,
-
-        nest_of_evil
-    ]
+    portraits = {
+        "City of Haze": PortraitGFX([], []),
+        "Sandy Grave": PortraitGFX([], []),
+        "Nation of Fools": PortraitGFX([], []),
+        "Forest of Doom": PortraitGFX([], []),
+        "13th Street": PortraitGFX([], []),
+        "Forgotten City": PortraitGFX([], []),
+        "Burnt Paradise": PortraitGFX([], []),
+        "Dark Academy": PortraitGFX([], []),
+        "Nest of Evil": PortraitGFX([], [])
+    }
 
     #  Read the regular portraits
     for i in range(4):
+        portrait = portraits[sprite_order[i]]
         sprite_start = (i // 2) * 0x2000  # The bottom portraits are 0x2000 bytes down
         for j in range(62):  # Portraits are 62 pixels tall
             address = 0x89 + sprite_start  # Read the start of the sprite
             address += (0x40 * (i % 2))  # Make sure we read the correct painting
             address += (0x80 * j)  # Each row is 0x80 bytes apart
-
-            portrait_order[i].append(rom.read_from_file(address, "portrait_set_1", 0x2E))
+            portrait.sprite.append(rom.read_from_file(address, "portrait_set_1", 0x2E))
 
     #  Also read the Remix portraits
     for i in range(4):
+        portrait = portraits[sprite_order[i + 4]]
         sprite_start = (i // 2) * 0x2000  # The bottom portraits are 0x2000 bytes down
         for j in range(62):  # Portraits are 62 pixels tall
             address = 0x89 + sprite_start  # Read the start of the sprite
             address += (0x40 * (i % 2))  # Make sure we read the correct painting
             address += (0x80 * j)  # Each row is 0x80 bytes apart
-
-            portrait_order[i + 4].append(rom.read_from_file(address, "portrait_set_2", 0x2E))
+            portrait.sprite.append(rom.read_from_file(address, "portrait_set_2", 0x2E))
 
     for i in range(62):  # Nest of evil is the only portrait we need out of Set 3
-        nest_of_evil.append(rom.read_from_file(0xC9 + (0x80 * i), "portrait_set_3", 0x2E))
+        portraits["Nest of Evil"].sprite.append(rom.read_from_file(0xC9 + (0x80 * i), "portrait_set_3", 0x2E))
 
     #  Read all of the shuffled Portrait destinations
-    hub_destination = portrait_data["City of Haze"].destination_pointer
-    stairs_underground = portrait_data["Sandy Grave"].destination_pointer
-    stairs_main = portrait_data["Nation of Fools"].destination_pointer
-    tower_destination = portrait_data["Forest of Doom"].destination_pointer
-    brauner_1 = portrait_data["Forgotten City"].destination_pointer
-    brauner_2 = portrait_data["13th Street"].destination_pointer
-    brauner_3 = portrait_data["Burnt Paradise"].destination_pointer
-    brauner_4 = portrait_data["Dark Academy"].destination_pointer
-    nest_destination = portrait_data["Nest of Evil"].destination_pointer
+    for portrait in portraits:
+        destination = portrait_data[portrait].destination_pointer
+        new_map = rom.read_from_file(destination[0] + 8, destination[1], 1)[0]
+        portraits[portrait].destination_map = new_map
 
-    hub_destination = rom.read_from_file(hub_destination[0] + 8, hub_destination[1], 1)[0]
-    stairs_underground = rom.read_from_file(stairs_underground[0] + 8, stairs_underground[1], 1)[0]
-    stairs_main = rom.read_from_file(stairs_main[0] + 8, stairs_main[1], 1)[0]
-    tower_destination = rom.read_from_file(tower_destination[0] + 8, tower_destination[1], 1)[0]
-    brauner_1 = rom.read_from_file(brauner_1[0] + 8, brauner_1[1], 1)[0]
-    brauner_2 = rom.read_from_file(brauner_2[0] + 8, brauner_2[1], 1)[0]
-    brauner_3 = rom.read_from_file(brauner_3[0] + 8, brauner_3[1], 1)[0]
-    brauner_4 = rom.read_from_file(brauner_4[0] + 8, brauner_4[1], 1)[0]
-    nest_destination = rom.read_from_file(nest_destination[0] + 8, nest_destination[1], 1)[0]
-
-    map_order = [
-        city_of_haze,
-        thirteenth_street,
-        sandy_grave,
-        forgotten_city,
-        nation_of_fools,
-        burnt_paradise,
-        forest_of_doom,
-        dark_academy,
-        nest_of_evil
-    ]
-
-    portrait_warps = [
-        hub_destination,
-        stairs_underground,
-        stairs_main,
-        tower_destination,
-        brauner_2,
-        brauner_1,
-        brauner_3,
-        brauner_4,
-        nest_destination
-    ]
-
-    for index, sprite in enumerate(portrait_order):
-        destination = portrait_warps[index] - 1 # Check the destination
-        if destination == 8:
-            destination = 9  # Nest of evil, shift the index up one
-
+    for index, portrait in enumerate(portraits):
+        destination = portraits[portrait].destination_map
+        destination = area_map[destination - 1]
+        sprite = portraits[destination].sprite
+        print(f"{portrait}: {index}")
+        if index == 8:
+            index = 9  # Bump up Nest because we skip over the real portrait 9
         for row in sprite:
             for i, pixel in enumerate(row):
-                if index % 2 and pixel < 0x80:  # This portrait is part of Row 2
-                    row[i] = pixel + 0x80
-
-                elif not index % 2 and pixel >= 0x80:
-                    row[i] = pixel - 0x80
+                if index % 2:
+                    row[i] = pixel | 0x80
+                else:
+                    row[i] = pixel & 0x7F
 
     for i in range(4):
-        destination_map = portrait_warps[i] - 1
-        new_portrait = map_order[destination_map]
+        portrait = sprite_order[i]
+        destination = portraits[portrait].destination_map
+        destination = area_map[destination - 1]
+        new_portrait = portraits[destination].sprite
+
         sprite_start = (i // 2) * 0x2000
         for j in range(62):
             address = 0x89 + sprite_start
@@ -237,8 +234,11 @@ def adjust_portrait_gfx(rom):
             rom.write_to_file(address, "portrait_set_1", new_portrait[j])
 
     for i in range(4):
-        destination_map = portrait_warps[i + 4] - 1
-        new_portrait = map_order[destination_map]
+        portrait = sprite_order[i + 4]
+        destination = portraits[portrait].destination_map
+        destination = area_map[destination - 1]
+        new_portrait = portraits[destination].sprite
+        
         sprite_start = (i // 2) * 0x2000
         for j in range(62):
             address = 0x89 + sprite_start
@@ -246,53 +246,28 @@ def adjust_portrait_gfx(rom):
             address += (0x80 * j)
             rom.write_to_file(address, "portrait_set_2", new_portrait[j])
 
-    nest_portrait = portrait_warps[8]
-    nest_map = map_order[nest_portrait - 1]
+    nest_destination = portraits["Nest of Evil"].destination_map
+    nest_portrait = area_map[nest_destination - 1]
+    nest_portrait_sprite = portraits[nest_portrait].sprite
     for i in range(62):  # Nest of evil is the only portrait we need out of Set 3
-        rom.write_to_file(0xC9 + (0x80 * i), "portrait_set_3", nest_map[i])
-
-    # Get the palettes ##
-    city_of_haze = []
-    sandy_grave = []
-    nation_of_fools = []
-    forest_of_doom = []
-    thirteenth_street = []
-    forgotten_city = []
-    burnt_paradise = []
-    dark_academy = []
-    nest_of_evil = []
-
-    portrait_palettes = [
-        city_of_haze,
-        sandy_grave,
-        nation_of_fools,
-        forest_of_doom,
-        thirteenth_street,
-        forgotten_city,
-        dark_academy,
-        burnt_paradise,
-        nest_of_evil
-    ]
-
-    map_order = [
-        city_of_haze,
-        thirteenth_street,
-        sandy_grave,
-        forgotten_city,
-        nation_of_fools,
-        burnt_paradise,
-        forest_of_doom,
-        dark_academy,
-        nest_of_evil
-    ]
+        rom.write_to_file(0xC9 + (0x80 * i), "portrait_set_3", nest_portrait_sprite[i])
 
     for i in range(8):
-        portrait_palettes[i].append(rom.read_from_file((0x22BD6C4 + (0x3028 * (i // 4))) + (0x100 * (i % 4)), "overlay_7", 0x100))
+        portrait = palette_order[i]
+        portraits[portrait].palette.append(rom.read_from_file(
+            (0x22BD6C4 + (0x3028 * (i // 4))) + (0x100 * (i % 4)), "overlay_7", 0x100))
+    test_palette = portraits["Forest of Doom"].palette
+    for byte in test_palette[0]:
+        print(hex(byte))
 
-    portrait_palettes[8].append(rom.read_from_file(0x022C0BF0, "overlay_7", 0x100))  # Nest of evil
+    portraits["Nest of Evil"].palette.append(rom.read_from_file(0x022C0BF0, "overlay_7", 0x100))  # Nest of evil
 
     for i in range(8):
-        new_palette = map_order[portrait_warps[i] - 1][0]
+        base = sprite_order[i]
+        portrait = portraits[base].destination_map
+        new_map = area_map[portrait - 1]
+        new_palette = portraits[new_map].palette[0]
         rom.write_to_file((0x22BD6C4 + (0x3028 * (i // 4))) + (0x100 * (i % 4)), "overlay_7", new_palette)
 
-    #  62 pixels tall
+    palette = portraits[nest_portrait].palette
+    rom.write_to_file(0x022C0BF0, "overlay_7", palette[0])
