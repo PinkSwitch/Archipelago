6a1cee5dda2eca20560cb51a6eb40895834eea1b
hac52 <marcel.woelfle@outlook.de>
2026-09-11 03:34:36 +0200

    fix abyss_rule logic based on menace_triggers
    fix abyss_rule logic to handle menace_triggers conditionally.

--- worlds/cv_dos/Rules.py
+++ worlds/cv_dos/Rules.py
@@ -102,9 +102,11 @@ def set_location_rules(world: "DoSWorld") -> None:
     set_rule(world.get_location("Silenced Ruins: Mirror World"), Has("Paranoia Soul"))
     set_rule(world.get_location("Bat Company Soul"), Has(world.magic_seal_table["Silenced Ruins"]))
     set_rule(world.get_location("Silenced Ruins: Boss Room"), Has(world.magic_seal_table["Silenced Ruins"]))
-    abyss_rule = big_uppies & HasAll(*world.menace_triggers)
-    if not world.options.goal:
-        abyss_rule &= Has(world.magic_seal_table["The Pinnacle"])
+    if world.menace_triggers:
+        abyss_rule = big_uppies & HasAll(*world.menace_triggers)
+    else:
+        abyss_rule = big_uppies
+
 
     set_rule(world.get_location("Abyss Center"), abyss_rule)
     
