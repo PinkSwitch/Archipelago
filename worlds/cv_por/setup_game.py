b732b3b73d93a98faa5ccef148c325fd091b0402
PinkSwitch <blabidyfa@gmail.com>
2026-09-11 20:31:08 -0500

    Fix non-deterministic quest filler

--- worlds/cv_por/setup_game.py
+++ worlds/cv_por/setup_game.py
@@ -45,6 +45,6 @@ def place_static_items(world):
             continue
         world.get_location(quest).place_locked_item(create_item_as_event(world, quest_data[quest].vanilla_reward))
 
-    for quest in world.excluded_quests:
+    for quest in sorted(world.excluded_quests):
         #  Todo; Make this not call get filler or find some way to make this not call Magus Ring
         world.get_location(quest).place_locked_item(create_item_as_event(world, get_filler_item_name(world), True))
